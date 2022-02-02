da4b62c49e8d909378658cb9943680d5a9779b8c
Giuseppe Velocci <runnermean@gmail.com>
2022-02-02 07:52:17 +0100

    improved behaviour with resolver class

---
+++ DiContainerLibrary/Resolver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DiContainerLibrary
+{
+    class Resolver
+    {
+        public Func<object> Resolve { get; }
+        public ResolverType ResolverType { get; }
+
+        public Resolver(Func<object> resolve, ResolverType type)
+        {
+            Resolve = resolve;
+            ResolverType = type;
+        }
+    }
+
+    enum ResolverType
+    {
+        Singleton,
+        Transient
+    }
+}
